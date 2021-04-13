64c40e014b857c37596e843877ef9bbfaaa63831
JSpickelmier <78034029+JSpickelmier@users.noreply.github.com>
2021-04-13 09:38:10 -0500

    2nd commit Lab9

---
+++ Lab9/src/main/java/com/mycompany/lab9/Lab9Main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab9;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jacks
+ */
+public class Lab9Main { public static void main(String[] args) {
+    double len1 = 0;
+    double len2 = 0;
+    double width1 = 0;
+    double width2 = 0;
+    Scanner Keyboard = new Scanner(System.in);
+    
+    System.out.println("Hello! what is the length of your first Land Tract?");
+    len1 = Keyboard.nextInt();
+    
+     System.out.println("Hello! what is the width of your first Land Tract?");
+    width1 = Keyboard.nextInt();
+    
+     System.out.println("Hello! what is the length of your second Land Tract?");
+    len2 = Keyboard.nextInt();
+    
+     System.out.println("Hello! what is the width of your second Land Tract?");
+    width2 = Keyboard.nextInt();
+    
+    
+    LandTract LandTract1 = new LandTract(len1,width1);
+    LandTract LandTract2 = new LandTract(len2,width2);
+    
+    System.out.println("The area of Land Tract 1 is " + LandTract1.getArea() + " units");
+     System.out.println("The area of Land Tract 2 is " + LandTract2.getArea() + " units");
+    
+    }
+ 
+            
+    
+    
+    
+    
+    
+}
